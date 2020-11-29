4783ce1d5e2278ebd27edff51b662db6e2db0afd
Firas Naber <naber024@morris.umn.edu>
2020-11-29 11:22:01 +0200

    Implement partially working 'FileRetriever.java' file

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,25 +1,46 @@
 package segmentedfilesystem;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class FileRetriever {
 
-	public FileRetriever(String server, int port) {
-        // Save the server and port for use in `downloadFiles()`
-        //...
-	}
+        String server;
+        int port;
 
-	public void downloadFiles() {
-        // Do all the heavy lifting here.
-        // This should
-        //   * Connect to the server
-        //   * Download packets in some sort of loop
-        //   * Handle the packets as they come in by, e.g.,
-        //     handing them to some PacketManager class
-        // Your loop will need to be able to ask someone
-        // if you've received all the packets, and can thus
-        // terminate. You might have a method like
-        // PacketManager.allPacketsReceived() that you could
-        // call for that, but there are a bunch of possible
-        // ways.
-	}
+        public FileRetriever(String server, int port) {
+                // Save the server and port for use in `downloadFiles()`
+                this.server = server;
+                this.port = port;
+        }
+
+        public void downloadFiles() throws IOException {
+                InetAddress address = InetAddress.getByName(server);
+                byte[] buf = new byte[1028]; // Buffer with the size 1028
+
+                DatagramSocket sock = new DatagramSocket();
+
+                DatagramPacket pack = new DatagramPacket(buf, buf.length, address, port); // Starts connection
+                System.out.println("Connection established!");
+
+                sock.send(pack);
+
+                PacketManager manager = new PacketManager();
+
+                while (!manager.allPacketsRecieved()) { // When all files are received...
+                        byte[] buffer = new byte[1028];
+
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                        sock.receive(packet);
+
+                        System.out.println("Data received!");
+
+                        manager.newPacket(packet);
+                }
+                
+                sock.close();
+        }
 
 }
